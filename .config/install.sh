3709835b1100fec107956f5b86c79a6ebea9a1dc
Codie Cottrell <codiecottrell@gmail.com>
2024-02-04 19:50:11 -0500

    Update install.sh

--- .config/install.sh
+++ .config/install.sh
@@ -2,6 +2,7 @@
 ## Installation Script for setting up PDE
 
 # determine the OS
+# TODO: Prompt user for Mac or Linux distro
 
 ##### Install tools (MacOS specific) #####
 # if ($os=macOS)
@@ -12,17 +13,17 @@ echo "/bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/inst
 # depending on the OS select the correct installer
 $install='sudo brew install'
 
-# elseif (linux)
+# elseif (debian Linux)
 # $install='sudo apt install'
 
 #install moitoring tools and language managers
 echo "$install btop pyenv rbenv nvm"
 
-# install my neovim dependencies (telescope.nvim and mason.nvim respectively)
+# install my neovim dependencies (for telescope.nvim and mason.nvim respectively)
 echo "$install ripgrep wget"
 
 #install fonts (NEEDS TESTING)
 # echo "brew tap homebrew/cask-fonts && brew install --cask font-lekton-nerd-font"
 
 # Install Apps and Programs
-echo "$install obsidian prusaslicer"
+echo "$install obsidian prusaslicer orcaslicer"
